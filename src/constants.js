cd9f0f3835cbfa342316d4e43fd6fa24f6f4f54c
kuntal112 <kuntaldas.112@gmail.com>
2023-07-02 14:47:59 +0530

    some css changes made

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const API_URL="https://movieapp-1687452451773.azurewebsites.net";
-// export const API_URL="http://localhost:8081";
+// export const API_URL="https://movieapp-1687452451773.azurewebsites.net";
+export const API_URL="http://localhost:8081";
